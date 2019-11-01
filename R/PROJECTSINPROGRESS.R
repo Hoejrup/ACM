f9d4be559cb255f46d1999d49635f51c6835f1e2
Anders <andershoejrup@gmail.com>
2019-11-01 09:37:51 +0100

    Added a comment file, containing what we currently working on. You can here update, what you've done and what you are planning on doing.

---
+++ R/PROJECTSINPROGRESS.R
@@ -0,0 +1,21 @@
+### 
+# Create 1 or 2 more functions for Copula, look at summary and or print
+# Create general class for "simulations" including all the sub classes
+#
+#
+
+# Create summary for A-R
+# Summary for Inverse sampling
+# Christian can look into an "analysis" function, returning a 1page PDF with all results
+#   - Consider using the methods for generating the different pages
+#     - I.e. summary / plot
+# 
+# 
+# 
+# 
+# Remember to consider sub-classes. Can be specified in the following way
+# This is an example of an object data, with class simulation and subclass copula. It will first look for a method for copula.
+# class(data) <- c("copula", "simulation")
+#
+#
+#
