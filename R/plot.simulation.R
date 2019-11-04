640fe9eb8520854630401ce597a7744a10a270f2
Christian Skalborg Jensen <chr.skalborg@live.dk>
2019-11-04 10:55:26 +0100

    Added a plot function for the simulation class

---
+++ R/plot.simulation.R
@@ -0,0 +1,17 @@
+#' plot.simulation
+#' 
+#' @param x A result vector from a simulation algorithm
+#' 
+#' @return A plot of the summary of the input vector, containing mean, variance, relative error and confidence interval
+#' 
+#' @export
+plot.simulation <- function(x){
+  summ <- simulation.summary(x)
+  my_mean <- summ$Mean
+  my_REL <- summ$`Relative Error`
+  lower <- my_mean*(1-my_REL)
+  upper <- my_mean*(1+my_REL)
+  
+  plot(1, my_mean, ylim = c(lower*(1-my_REL), upper*(1+my_REL)))
+  points(c(1,1), c(lower,upper), col = 'blue')
+}
