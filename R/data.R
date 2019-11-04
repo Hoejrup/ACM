64eb7a3ab139510cb7d3f4922bb1b8c4f476b662
Christian Skalborg Jensen <chr.skalborg@live.dk>
2019-11-04 09:56:30 +0100

    Trying to get the documentation the work

--- R/data.R
+++ R/data.R
@@ -2,9 +2,11 @@
 #'
 #' A dataset containig simulated data. X and Y are dependant on each other, by the lurking variable Z.
 #'
-#' @format A column vector with 1000000 rows
+#' @format A data frame with 1000 rows and 3 variables:
 #'
 #' \describe{
-#'   \item{MC_pi}{Test}
+#'   \item{my_X}{Linear dependent on Z, with slope 1.2 and intercept 4.3. Further it contains a centered normal error term, with variance 2.4.}
+#'   \item{my_Y}{Linear dependent on Z, with slope 3.2 and intercept -2.1. Further it contains a centered normal error term, with variance 1.7.}
+#'   \item{my_Z}{1000 realizations of standard normal.}
 #' }
 "MC_pi"
