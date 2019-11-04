2a227f4a174bda2bbc89ecd66cf6fd2c8619ffe7
Christian Skalborg Jensen <chr.skalborg@live.dk>
2019-11-04 09:58:27 +0100

    Added function to summarize a simulation vector

---
+++ R/summary.simulation.R
@@ -0,0 +1,16 @@
+#' summary.simulation
+#' 
+#' @param x A result vector from a simulation algorithm
+#' 
+summary.simulation <- function(x){
+  summary <- vector("list", length = 0)
+  summary['Mean'] <- mean(x)
+  summary['Variance'] <- var(x)
+  summary['Relative Error'] <- qnorm(0.975)*sqrt(var(x)/length(x))/mean(x)
+  summary['Confidence interval'] <- paste("[",
+                                          mean(x)-qnorm(0.975)*var(x)/sqrt(length(x)),
+                                          ";",
+                                          mean(x)+qnorm(0.975)*var(x)/sqrt(length(x)),
+                                          "]")
+  print(summary)
+}
