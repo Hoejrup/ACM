3b52333e8169a42f31e08ea3a76461222e1c3729
Christian Skalborg Jensen <chr.skalborg@live.dk>
2019-11-04 09:36:36 +0100

    Added documentation of the pi simulated data

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Indirect dependence for lurkplot
+#'
+#' A dataset containig simulated data. X and Y are dependant on each other, by the lurking variable Z.
+#'
+#' @format A column vector with 1000000 rows
+#'
+#' \describe{
+#'   \item{MC_pi}{Test}
+#' }
+"MC_pi"
