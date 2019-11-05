a2034b51cf84a7b4038bd28c8d57b59522e6630a
Christian Skalborg Jensen <chr.skalborg@live.dk>
2019-11-05 10:23:06 +0100

    Added example

--- R/plot.simulation.R
+++ R/plot.simulation.R
@@ -4,6 +4,11 @@
 #' 
 #' @return A plot of the summary of the input vector, containing mean, variance, relative error and confidence interval
 #' 
+#' @examples 
+#' x <- MC_pi
+#' class(x) <- 'simulation'
+#' plot(x)
+#' 
 #' @export
 plot.simulation <- function(x){
   summ <- summary(x)
