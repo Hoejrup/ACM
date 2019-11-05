ee54641c5c8ae09063509948623b7e09d653e472
Christian Skalborg Jensen <chr.skalborg@live.dk>
2019-11-05 10:40:57 +0100

    Added ggplot option to the plot function for simulation

--- R/plot.simulation.R
+++ R/plot.simulation.R
@@ -17,6 +17,25 @@ plot.simulation <- function(x){
   lower <- my_mean*(1-my_REL)
   upper <- my_mean*(1+my_REL)
   
-  plot(1, my_mean, ylim = c(lower*(1-my_REL), upper*(1+my_REL)))
-  points(c(1,1), c(lower,upper), col = 'blue')
+  ans <- 0
+  if("ggplot2" %in% rownames(installed.packages()) && "gridExtra" %in% rownames(installed.packages())){
+    ans <- menu(c("Yes", "No"), title="Do you want to use ggplot for plotting?")
+  } 
+  if(ans == 1){
+    y <- data.frame(
+      ones <- c(1,1,1),
+      values = c(my_mean, lower, upper),
+      is_mean = c(1,0,0)
+    )
+    
+    ggplot2::ggplot(data = y, ggplot2::aes(x = ones, y = values, color = is_mean)) +
+      ggplot2::geom_point() + 
+      ggplot2::theme_classic() +
+      ggplot2::theme(legend.position = "none") + 
+      ggplot2::ylab('Value') +
+      ggplot2::xlab("Simulation")
+  } else{
+    plot(1, my_mean, ylim = c(lower*(1-my_REL), upper*(1+my_REL)))
+    points(c(1,1), c(lower,upper), col = 'blue')
+  }
 }
