d46f1ad592c6ebcca27c6eb2cf4d2f1d48659808
MarkusL96 <legat.markus@gmail.com>
2019-11-08 09:55:22 +0100

    plot change

--- R/plot.AR.R
+++ R/plot.AR.R
@@ -15,12 +15,8 @@
 #' plot(ARsimulation)
 #' @export
 
-plot.AR <- function(ARsample){
-  y <- 0
-  if("ggplot2" %in% rownames(installed.packages()) && "gridExtra" %in% rownames(installed.packages())){
-    y <- menu(c("Yes", "No"), title="Do you want to use ggplot for plotting?")
-  } 
-  if(y == 1){
+plot.AR <- function(ARsample, ggplot = FALSE){
+  if(ggplot == TRUE){
     x <- seq(min(ARsample$samples),max(ARsample$samples),0.01)
     data1 <- data.frame(x = x, y = ARsample$"proposal density"(x), z = ARsample$"target density"(x))
     data2 <- data.frame(x = ARsample$samples)
@@ -35,14 +31,35 @@ plot.AR <- function(ARsample){
       ggplot2::ylab("empirical vs. theoretical density") + 
       ggplot2::scale_colour_manual("", breaks = c("empirical target density", "theoretical target density"), values = c("empirical target density" ="green", "theoretical target density"="red"))
     gridExtra::grid.arrange(plot1, plot2, ncol=2)
-  } else{
-  par(mfrow = c(1,2))
-  x <- seq(min(ARsample$samples),max(ARsample$samples),0.01)
-  plot(x, ARsample$"proposal density"(x), type = "l", main = "target vs. proposal", 
-       xlab = "", ylab = "")
-  lines(x, ARsample$"target density"(x), col = "red")
-  plot(density(ARsample$samples), main = "theoretical vs empirical proposal", xlab = "", ylab = "")
-  lines(x, ARsample$"target density"(x), col = "red")
-  par(mfrow = c(1,1))
+  } else {
+    y <- 0
+    if("ggplot2" %in% rownames(installed.packages()) && "gridExtra" %in% rownames(installed.packages())){
+      y <- menu(c("Yes", "No"), title="Do you want to use ggplot for plotting?")
+    } 
+    if(y == 1){
+      x <- seq(min(ARsample$samples),max(ARsample$samples),0.01)
+      data1 <- data.frame(x = x, y = ARsample$"proposal density"(x), z = ARsample$"target density"(x))
+      data2 <- data.frame(x = ARsample$samples)
+      plot1 <- ggplot2::ggplot(data1, ggplot2::aes(x)) + 
+        ggplot2::geom_line(ggplot2::aes(y = y, colour = "proposal density")) + 
+        ggplot2::geom_line(ggplot2::aes(y = z, colour = "target density")) + 
+        ggplot2::ylab("proposal vs. target density") + 
+        ggplot2::scale_colour_manual("", values = c("proposal density" ="green", "target density"="red"))
+      plot2 <- ggplot2::ggplot(data2, ggplot2::aes(x)) + 
+        ggplot2::stat_density(ggplot2::aes(colour = "empirical target density"),geom="line") + 
+        ggplot2::geom_line(data = data1, ggplot2::aes(x, y = z, colour = "theoretical target density")) + 
+        ggplot2::ylab("empirical vs. theoretical density") + 
+        ggplot2::scale_colour_manual("", breaks = c("empirical target density", "theoretical target density"), values = c("empirical target density" ="green", "theoretical target density"="red"))
+      gridExtra::grid.arrange(plot1, plot2, ncol=2)
+    } else{
+      par(mfrow = c(1,2))
+      x <- seq(min(ARsample$samples),max(ARsample$samples),0.01)
+      plot(x, ARsample$"proposal density"(x), type = "l", main = "target vs. proposal", 
+           xlab = "", ylab = "")
+      lines(x, ARsample$"target density"(x), col = "red")
+      plot(density(ARsample$samples), main = "theoretical vs empirical proposal", xlab = "", ylab = "")
+      lines(x, ARsample$"target density"(x), col = "red")
+      par(mfrow = c(1,1))
+    }
   }
 }
