068dbccdefc0449bd37de8cb1e42a38e7df921f5
Anders <andershoejrup@gmail.com>
2019-11-08 13:24:41 +0100

    Added more details to the vignette about copula and bivariate normal. Also updated plot.copula to take input ggplot=T.

--- R/plot.copula.R
+++ R/plot.copula.R
@@ -23,21 +23,27 @@
 #' Y <- R*(1-U)
 #' data <- matrix(c(X,Y),ncol=2)
 #' class(data) <- 'copula'
-#' plot(data)
+#' plot(data, ggplot=T)
 #' 
 #' 
 #' @export
 
 #plot copula using ECDF
-plot.copula=function(data){
+plot.copula=function(data, ggplot = FALSE){
   xo=rank(data[,1])  
   yo=rank(data[,2])  
   n=length(data[,1])
-  if("ggplot2" %in% rownames(installed.packages()) == T){
+  if(ggplot == T){
+    data <- data.frame(x = data[,1], y = data[,2])
+    ggplot2::ggplot(data=data, ggplot2::aes(xo/n, yo/n)) + 
+      ggplot2::geom_jitter(color = 'Black', size=0.1) +
+      ggplot2::xlab("Rank(X)/n") +
+      ggplot2::ylab("Rank(Y)/n")
+  }
+  if(ggplot == F && "ggplot2" %in% rownames(installed.packages()) == T){
     y <- menu(c("Yes", "No"), title="You have installed ggplot2. Do you want to use it for plotting?")
   }
   if(y == 1){
-    library(ggplot2)
     data <- data.frame(x = data[,1], y = data[,2])
     ggplot2::ggplot(data=data, ggplot2::aes(xo/n, yo/n)) + 
       ggplot2::geom_jitter(color = 'Black', size=0.1) +
