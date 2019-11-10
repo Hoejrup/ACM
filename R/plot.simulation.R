502484740101b6492f9c4c617d9035d88fc7bb8a
chrskalborg <chr.skalborg@live.dk>
2019-11-10 13:52:51 +0100

    Updated simualtion plot, to accept ggplot input from function call

--- R/plot.simulation.R
+++ R/plot.simulation.R
@@ -1,7 +1,7 @@
 #' plot.simulation
 #' 
 #' @param x A result vector from a simulation algorithm
-#' 
+#' @param ggplot A boolean variable, determining if to use ggplot, directly from function call, rather than using control input
 #' @return A plot of the summary of the input vector, containing mean, variance, relative error and confidence interval
 #' 
 #' @examples 
@@ -10,18 +10,15 @@
 #' plot(x)
 #' 
 #' @export
-plot.simulation <- function(x){
+plot.simulation <- function(x, ggplot = FALSE){
+
   summ <- summary(x, print = F)
   my_mean <- summ$Mean
   my_REL <- summ$`Relative Error`
   lower <- my_mean*(1-my_REL)
   upper <- my_mean*(1+my_REL)
   
-  ans <- 0
-  if("ggplot2" %in% rownames(installed.packages()) && "gridExtra" %in% rownames(installed.packages())){
-    ans <- menu(c("Yes", "No"), title="Do you want to use ggplot for plotting?")
-  } 
-  if(ans == 1){
+  if(ggplot == T){
     y <- data.frame(
       ones <- c(1,1,1),
       values = c(my_mean, lower, upper),
@@ -34,8 +31,27 @@ plot.simulation <- function(x){
       ggplot2::theme(legend.position = "none") + 
       ggplot2::ylab('Value') +
       ggplot2::xlab("Simulation")
-  } else{
-    plot(1, my_mean, ylim = c(lower*(1-my_REL), upper*(1+my_REL)))
-    points(c(1,1), c(lower,upper), col = 'blue')
+  }else{
+    ans <- 0
+    if("ggplot2" %in% rownames(installed.packages()) && "gridExtra" %in% rownames(installed.packages())){
+      ans <- menu(c("Yes", "No"), title="Do you want to use ggplot for plotting?")
+    } 
+    if(ans == 1){
+      y <- data.frame(
+        ones <- c(1,1,1),
+        values = c(my_mean, lower, upper),
+        is_mean = c(1,0,0)
+      )
+      
+      ggplot2::ggplot(data = y, ggplot2::aes(x = ones, y = values, color = is_mean)) +
+        ggplot2::geom_point() + 
+        ggplot2::theme_classic() +
+        ggplot2::theme(legend.position = "none") + 
+        ggplot2::ylab('Value') +
+        ggplot2::xlab("Simulation")
+    } else{
+      plot(1, my_mean, ylim = c(lower*(1-my_REL), upper*(1+my_REL)))
+      points(c(1,1), c(lower,upper), col = 'blue')
+    }
   }
 }
